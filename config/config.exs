3a30096f551b93c0882478e6908ca5cf9f976847
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2022-08-16 19:54:55 -0300

    Replace `use Mix.Config` by `import Config`

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
 # Stop lager redirecting :error_logger messages
 config :lager, :error_logger_redirect, false
@@ -8,6 +8,6 @@ config :lager, :error_logger_redirect, false
 # Stop lager removing Logger's :error_logger handler
 config :lager, :error_logger_whitelist, [Logger.ErrorHandler]
 
-if Mix.env() == :test do
+if config_env() == :test do
   config :conduit, ConduitAMQPTest, url: System.get_env("AMQP_URL") || "amqp://guest:guest@localhost"
 end
