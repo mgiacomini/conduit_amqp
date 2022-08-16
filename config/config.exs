663e51b7c0fc26dd68ead8879984f963e8b6867e
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2022-08-16 19:57:46 -0300

    Remove lagger config after amqp upgrade

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,6 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
 import Config
 
-# Stop lager redirecting :error_logger messages
-config :lager, :error_logger_redirect, false
-
-# Stop lager removing Logger's :error_logger handler
-config :lager, :error_logger_whitelist, [Logger.ErrorHandler]
-
 if config_env() == :test do
-  config :conduit, ConduitAMQPTest, url: System.get_env("AMQP_URL") || "amqp://guest:guest@localhost"
+  config :conduit, ConduitAMQPTest, 
+    url: System.get_env("AMQP_URL", "amqp://guest:guest@localhost")
 end
